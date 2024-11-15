4124e2cbffe07d48271df5da8fedc7a75f29cefb
mona-kumar <56031334+MonaSweataSK@users.noreply.github.com>
2024-11-15 15:25:03 +0530

    browser router import

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header  from './components/Header';
  import MarketingApp  from './components/MarketingApp';
-import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import { BrowserRouter } from 'react-router-dom';
 
 
 export default() =>  {
